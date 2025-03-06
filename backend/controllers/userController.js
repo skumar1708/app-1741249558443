71997d140531e76772f28a1ac0922113ebefb6bc
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-06 13:57:56 +0530

    Automated commit: backend/controllers/userController.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,26 @@
+const User = require('../models/User');
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createUser = async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getUsers,
+  createUser
+};
